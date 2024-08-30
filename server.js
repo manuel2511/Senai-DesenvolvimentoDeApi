8f84146a805391141f704edbb5e1113b040ed9bb
manuel2511 <tekmanuel@gmail.com>
2024-08-29 21:34:40 -0300

    Add PUT

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.post("/cliente/novo", (req, res) => {
   };
   res.status(201);
   res.json(response);
-});
+}); 
+app.put("/cliente/update/cpfcnpj/12345678901", (req, res) => {
+  const response = {
+    mesagem: "Dados atualizados com sucesso",
+    status: 200,
+  };
+  res.status(200);
+  res.json(response);
+}); 
 
 app.listen(port, () => {
   console.log("Servidor em execução na porta ", port);
